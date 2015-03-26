0e51fc059af24037b0374cca2e74e0ee53f06442
hiro <hiromartweb@gmail.com>
2015-03-26 21:17:32 +0900

    3Dシューティングゲーム

---
+++ Assets/MyFolder/Script/CameraControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    public Vector3 difference;
+    float differenceX;
+
+	// Use this for initialization
+	void Start () {
+
+        difference = transform.localPosition;
+        differenceX = transform.localPosition.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if (GameObject.Find("Main Camera").GetComponent<GameControl>().gameFlag == true) {
+            if (GameObject.Find("SciFi_Fighter_AK5(Clone)") == true) {
+                Vector3 startVec = GameObject.Find("SciFi_Fighter_AK5(Clone)").transform.localPosition;
+                transform.localPosition = new Vector3(differenceX, startVec.y + difference.y, startVec.z + difference.z);
+            }
+        //}
+	
+	}
+}
